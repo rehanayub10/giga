e6ae6d11c8c6393856242059c8ad03b2ce7e7f44
rehanayub10 <rehanayub20@gmail.com>
2022-07-20 00:05:04 -0400

    update raw mode

--- giga.c
+++ giga.c
@@ -1,35 +1,49 @@
+/*** includes ***/
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
 #include <termios.h>
 
+/*** data ***/
+
 struct termios orig_termios;
 
+/*** terminal ***/
+void die (const char* s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode); //this can be called from anywhere
     
     struct termios raw = orig_termios;
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST); //no default output processing
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); //turn off echo, canonical, CTRL sigs
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 10;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr"); 
 }
+
+/*** init ***/
 
 int main() {
     enableRawMode();
     
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die ("read");
         if iscntrl(c) {
             printf("%d \r\n", c);
         } else {
