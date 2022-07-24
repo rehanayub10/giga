76065f9f6769e4c083a3c3c3361cf13ed2ff7668
rehanayub10 <rehanayub20@gmail.com>
2022-07-23 21:19:07 -0400

    base raw mode functionality done

--- giga.c
+++ giga.c
@@ -1,17 +1,31 @@
+/*** includes ***/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
+/*** defines ***/
+#define CTRL_KEY(k) (k & 0x1f)
+
+/*** data ***/
+
 struct termios orig_termios;
 
+/*** terminal ***/
+
+void die(const char* str) {
+    perror(str);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(disableRawMode);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    atexit(disableRawMode); //needs to have return type of void
     
     struct termios termios_p = orig_termios;
     termios_p.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP
@@ -20,18 +34,24 @@ void enableRawMode() {
     termios_p.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
     termios_p.c_cflag &= ~(CSIZE | PARENB);
     termios_p.c_cflag |= CS8;
+    termios_p.c_cc[VMIN] = 0;
+    termios_p.c_cc[VTIME] = 10;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_p);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_p) == -1) die("tcsetattr");
 }
+
+/*** input ***/
+
+/*** init ***/
 
 int main() {
     enableRawMode();
     
-    char c;
     while (1) {
+        char c = '\0';
         read(STDIN_FILENO, &c, 1);
+        if (c == CTRL_KEY('q')) break;
         printf("%c\r\n", c);
-        if (c == 'q') break;
     }
     
     return 0;
